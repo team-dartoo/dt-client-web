1dbf930739fe4107919842c82613545e524d8d2b
Dyeon16 <leedy1016@naver.com>
2025-11-04 19:22:38 +0900

    메인화면 카드작업 수정

--- src/pages/Bookmark/Bookmark.jsx
+++ src/pages/Bookmark/Bookmark.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import NavBar from "../../shared/components/Navbar";
 import Header from "../../shared/components/Header";
+import chatIcon from "@/images/chat_icon.svg";
+import dartooLogo from "@/images/DARTOO.svg";
 
 const Bookmark = () => {
   return (
